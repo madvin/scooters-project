62d31a79207c22a7a545c01509a349a2dcae2007
madvin <madvin@abv.bg>
2025-03-23 10:27:37 +0200

    modified:   client/index.html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import Footer from './components/Footer';
-
 
 function App() {
   return (
