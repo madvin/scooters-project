118ca5f914a16a206acef9bf05b29415cb599e2d
madvin <madvin@abv.bg>
2025-03-30 11:53:41 +0300

    useAuth for authentication

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -4,3 +4,24 @@ import request from '../utils/request';
 export default function useAuth() {
 
     const authData = useContext(UserContext);
+    const requestWrapper = (method, url, data, options = {}) => {
+        const authOptions = {
+            ...options,
+            headers: {
+                ...options.headers,
+                'X-Authorization': authData.accessToken,
+            }
+        };
+        return request.baseRequest(method, url, data, authData.accessToken ? authOptions : options);
+    };
+
+    return {
+        ...authData,
+        request: {
+            get: requestWrapper.bind(null, 'GET'),
+            post: requestWrapper.bind(null, 'POST'),
+            put: requestWrapper.bind(null, 'PUT'),
+            delete: requestWrapper.bind(null, 'DELETE'),
+        }
+    }
+};
