bbaf5e6a518b7767cc3b5618c738b1f895abc895
madvin <madvin@abv.bg>
2025-03-30 13:53:35 +0300

    add div and main container to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Login from './components/Login';
-import Register from './components/Register';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { UserContext } from "./contexts/UserContext";
+import { authData } from "./hooks/useAuth";
+import { userLoginHandler, userLogoutHandler } from "./hooks/useAuth";
 
 function App() {
   return (
-    <>
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      <Footer />
-    </>
+    <UserContext.Provider
+      value={{ ...authData, userLoginHandler, userLogoutHandler }}
+    >
+      <div id="box">
+        <Header />
+        <main>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+          <Footer />
+        </main>
+      </div>
+    </UserContext.Provider>
   );
 }
 
