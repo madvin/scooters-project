b45f64b80610b055f648d71ae1995b1a595b9b13
madvin <madvin@abv.bg>
2025-04-03 19:44:39 +0300

    remove some imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Box, useTheme } from '@mui/material';
+import { Box } from '@mui/material';
 import { ToastContainer } from 'react-toastify';
 
 import UserProvider from './providers/UserProvider';
